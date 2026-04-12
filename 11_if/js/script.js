b7be76afecd35481135b250c0203dfcee3e167d3
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-12 11:55:10 -0300

    if

---
+++ 11_if/js/script.js
@@ -0,0 +1,11 @@
+var idade = 16;
+console.log("antes do if");
+
+
+if(idade >= 18){
+    console.log("Está apto para tirar a carteira de motorista");
+}
+else{
+    console.log("Não está apto para tirar a carteira de motorista");
+}
+console.log("depois do if");
