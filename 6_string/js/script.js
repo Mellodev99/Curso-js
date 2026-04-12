9f0ba13e9e33fa652e7bc8c97377bdb8a44c5a98
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-11 23:22:29 -0300

    .

---
+++ 6_string/js/script.js
@@ -0,0 +1,27 @@
+var nome = "luan";
+var sobrenome = "Mello";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+
+
+console.log(nomeCompleto);
+
+console.log( typeof "5.958"); //numeros entre aspas são string, mesmo que sejam numeros.    
+
+var frase = "Eu vou ser um grande programador do banco do Brasil!!";
+
+console.log(frase);
+console.log(typeof frase);
+
+document.write(frase);
+
+document.write("<br>");
+
+document.write('Ele disse: "Olá, tudo bem?"'); //para usar aspas dentro de aspas, basta usar aspas simples para envolver a string.
+
