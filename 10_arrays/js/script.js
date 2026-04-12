67f6d477feb5956158cd11089c46d35c276dda5a
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-12 00:24:09 -0300

    array

---
+++ 10_arrays/js/script.js
@@ -0,0 +1,16 @@
+var arr = [10, "luan", true, null, undefined, {name: "luan"}, [1, 2, 3]];
+
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[1]);
+
+
+arr[0] = 20;
+
+arr[4] = "teste";
+
+
+console.log(arr);
+
+console.log(typeof arr);
