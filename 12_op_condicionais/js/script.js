c9f5d7f774fdbf833d545cfd7047fdf1da9e6168
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-12 12:10:22 -0300

    /

---
+++ 12_op_condicionais/js/script.js
@@ -0,0 +1,20 @@
+var idade = 18;
+var possuiCarro = 0;
+
+
+if (idade >=18){
+    console.log("Está apto para tirar a carteira de motorista");
+}
+
+if(idade <= 17){
+    console.log("Não está apto para tirar a carteira de motorista");
+} 
+if(possuiCarro ){
+    console.log("Pode dirigir");
+}
+
+var nome = "Luan";
+    
+if(nome == "Luan"){
+    console.log("Olá, Luan");
+}
