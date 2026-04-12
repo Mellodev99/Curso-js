14cb43c7bed673d2b0771b869976e25032471dca
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-11 21:49:36 -0300

    variaveis

---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,21 @@
+var teste = 1;
+var nome = "Luan";
+
+console.log(teste);
+console.log(nome);
+
+teste = "Mellodev";
+
+console.log(teste);
+
+var meuSobrenome = "Mellodev";
+var meuNomeCompleto = "Luan Mellodev";
+
+console.log(meuSobrenome);
+console.log(meuNomeCompleto);
+
+let idade = 19;
+console.log(idade);
+
+const pi = 3.14;
+console.log(pi);
