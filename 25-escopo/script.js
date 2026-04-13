ce31a65ccbd1e78fb6acd0b86bfc7a465d832266
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-12 21:37:24 -0300

    .

--- 25-escopo/script.js
+++ 25-escopo/script.js
@@ -1,27 +1,27 @@
-var lista = ["Maçã", "Banana", "Laranja", "Uva", "Abacaxi"];
+var x = 1;
 
-var listaUl = document.createElement('ul');
+var y = 3;
 
-var body = document.getElementsByTagName('body');
+console.log(x, y);
 
-//console.log(body[0]);
+function teste(){
+    var z = 0;
 
-console.log(body[0]);
-
-body[0].appendChild(listaUl);
-
-var listaNoBody = document.getElementsByTagName('ul');
-
-console.log(listaNoBody[0]);
-
-for (var i = 0; i < lista.length; i++) {
-    
-    var liFor = document.createElement('li');
-
-    var textLi = document.createTextNode(lista[i]);
-
-    liFor.appendChild(textLi); //adicionando o texto pra dentro do li
-
-    listaNoBody[0].appendChild(liFor);
+    console.log(z);
+    console.log(x);
 }
 
+teste();
+function testando (){
+     var z = 5;
+
+     console.log(z);
+}
+
+testando();
+
+if(true){
+    var p = 1;
+}
+
+console.log(p);
