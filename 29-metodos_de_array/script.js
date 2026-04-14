47562364d63b5d199a17f797d1643688a1a6ef27
Luan Melo dos santos. <luanmello350@gmail.com>
2026-04-13 22:41:04 -0300

    .

---
+++ 29-metodos_de_array/script.js
@@ -0,0 +1,23 @@
+// length
+
+
+var nome = "Luan";
+
+console.log(nome.length);
+
+var obj = "bola";
+
+console.log(obj.length);
+
+console.log(nome[3]);
+
+var frase = "Eu vou ser um grande programador!";
+
+console.log(frase.indexOf("programador"));
+
+
+//slice
+
+var programador = frase.slice(21,32);
+
+console.log(programador);
